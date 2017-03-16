4a785a4216094490c39ac4b33575e13132dda79c
Cristian Sena <csena72@gmail.com>
2017-03-16 11:35:51 -0300

    cambios varios

--- modules/custom/noticias/src/Controller/NoticiasController.php
+++ modules/custom/noticias/src/Controller/NoticiasController.php
@@ -2,28 +2,26 @@
 
 namespace Drupal\noticias\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
 /**
  * 
  */
-class NoticiasController
-{
-   /* 
-    function __construct(argument)
-    {
-        # code...
-    }*/
-
-    function description()
-    {
+class NoticiasController extends ControllerBase {
+   
+    function description(){
+        $url = Url::fromRoute('block.admin_display');
+        $block_admin_link = $this->l($this->t('Pagina de administración de bloques'), $url);
+        
         $build = array(
             '#type' => 'markup',
-            '#markup' => '<p>'. t('Noticias ofrece un bloque en el que se mostraran las noticicas').'</p>',
+            '#markup' => $this->t('<p>Noticias ofrece un bloque en el que se mostraran las noticicas</p><p>').$block_admin_link.'</p>',
         );
         return $build;
     }
 
-    function getNoticia($idNoticia, $vista)
-    {
+    function getNoticia($idNoticia, $vista){
         $build = array(
             '#type' => 'markup',
             '#markup' => '<p>'. t('Esta es la página que muestra noticias con la noticia con id ').'<strong>'.$idNoticia.'</strong>'. t(' y con la vista ').'<strong>'.$vista.'</strong>'.'</p>',
