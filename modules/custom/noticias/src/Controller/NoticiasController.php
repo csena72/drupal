bdbde70c19722fa54c853b9efda42a11ac39d238
Cristian Sena <csena72@gmail.com>
2017-03-16 11:39:24 -0300

    formato

--- modules/custom/noticias/src/Controller/NoticiasController.php
+++ modules/custom/noticias/src/Controller/NoticiasController.php
@@ -6,11 +6,13 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Url;
 
 /**
- * 
+ *
  */
-class NoticiasController extends ControllerBase {
+class NoticiasController extends ControllerBase
+{
    
-    function description(){
+    function description()
+    {
         $url = Url::fromRoute('block.admin_display');
         $block_admin_link = $this->l($this->t('Pagina de administración de bloques'), $url);
         
@@ -21,7 +23,8 @@ class NoticiasController extends ControllerBase {
         return $build;
     }
 
-    function getNoticia($idNoticia, $vista){
+    function getNoticia($idNoticia, $vista)
+    {
         $build = array(
             '#type' => 'markup',
             '#markup' => '<p>'. t('Esta es la página que muestra noticias con la noticia con id ').'<strong>'.$idNoticia.'</strong>'. t(' y con la vista ').'<strong>'.$vista.'</strong>'.'</p>',
